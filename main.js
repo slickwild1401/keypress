ccbfa43f8658a3bd748d8b66f806b7589ee37abd
slickwild1401 <116937194+slickwild1401@users.noreply.github.com>
2023-06-03 16:03:26 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -31,36 +31,57 @@ function my_keydown(e) {
 		alphabetkey();
 		document.getElementById("d1").innerHTML = "You pressed Alphabet Key";
 		console.log("Alphabet Key");
-	} else {
+	} else if (keyPressed >= 48 && keyPressed <= 57) {
+		numberkey();
+		document.getElementById("d1").innerHTML = "You pressed Number Key";
+		console.log("Number Key");
+	} else if (keyPressed >= 37 && keyPressed <= 40) {
+
+		arrowkey();
+		document.getElementById("d1").innerHTML = "You pressed Arrow Key";
+		console.log("ArrowKey");
+
+	} else if ( keyPressed = 17, 18, 27){
+           specialkey();
+		   document.getElementById("d1").innerHTML = "You pressed Special Key";
+		   console.log("Special key");
+	}
+	else {
 		otherkey();
 		document.getElementById("d1").innerHTML = "You pressed symbol or other key";
+
 	}
-	
-	}
+
+
+
+}
 
 
 
-	function alphabetkey() {
-		img_image = "Alpkey.png";
-		add();
-	}
 
-	function numberkey() {
-		img_image = "numkey.png";
-		add();
-	}
 
-	function arrowkey() {
-		img_image = "Arrkey.png";
-		add();
-	}
 
-	function specialkey() {
-		img_image = "spkey.png";
-		add();
-	}
+function alphabetkey() {
+	img_image = "Alpkey.png";
+	add();
+}
 
-	function otherkey() {
-		img_image = "otherkey.png";
-		add();
-	}
+function numberkey() {
+	img_image = "numkey.png";
+	add();
+}
+
+function arrowkey() {
+	img_image = "Arrkey.png";
+	add();
+}
+
+function specialkey() {
+	img_image = "spkey.png";
+	add();
+}
+
+function otherkey() {
+	img_image = "otherkey.png";
+	add();
+}
